64c847af03d707209632b6dac71fb70a495799a8
Shane Osbourne <shane.osbourne8@gmail.com>
2018-03-03 22:07:59 +0000

    cli

--- readme.md
+++ readme.md
@@ -1,8 +1,54 @@
 ## Todo
 
+## `npm install -g swagger-ts`
+
+> Automatically generate Typescript namespaces from Swagger definitions
+
+<details>
+  <summary>Click to see example output</summary>
+    
+```typescript
+export namespace AcmeManageBasketAddToBasketV1Put {
+  export const description = "";
+  export const method = "PUT";
+  export const operationId = "acmeManageBasketAddToBasketV1AddProductPut";
+  export const path = "/V1/baskets/mine/add";
+  export interface Body {
+      productId: number;
+      qty: number;
+      purchaseType: string;
+  }
+  export type Response = Response200 | Response401 | ResponseDefault;
+  export type Response200 = string;
+  export type Response401 = Definitions.AcmeErrorResponse;
+}
+export namespace Definitions {
+  export interface AcmeErrorResponse {
+      message: string;
+  }
+}
+```
+
+</details>
+
+## Quick-start
+```bash
+# install
+npm install -g swagger-ts
+# or 
+yarn global add swagger-ts
+
+# run against a single JSON file
+swagger-ts dir/myfile.json
+
+# or pipe json to it
+curl http://some.swagger.api/schema | swagger-ts --stdin
+```
+
 [x] - GET url params 
 [x] - simpler local refs in definitions
 [x] - array refs
 [x] - test + lint + build in npm test
-[ ] - CLI
+[x] - CLI stdin
+[x] - CLI files
 [ ] - support multiple file outputs
---
+++ src/bin.ts
@@ -0,0 +1,137 @@
+#!/usr/bin/env node
+import {existsSync, readFileSync} from "fs";
+import stdin = require("get-stdin");
+import {OrderedSet} from "immutable";
+import minimist = require("minimist");
+import {join, parse, ParsedPath} from "path";
+import {createDefs} from "./swagger";
+
+const argv = minimist(process.argv.slice(2));
+
+// unique input
+const inputs = OrderedSet<string>(argv._);
+
+// defaults
+const defaults = {};
+
+// merged options with defaults
+const options = {
+    ...defaults,
+    ...argv,
+};
+
+if (options.stdin) {
+    stdin().then((str: string) => {
+        if (str === "") {
+            // tslint:disable-next-line
+            console.error("no input provided");
+        } else {
+            try {
+                // tslint:disable-next-line
+                console.log(createDefs(JSON.parse(str)));
+            } catch (e) {
+                // tslint:disable-next-line
+                console.error("Invalid JSON");
+                // tslint:disable-next-line
+                console.error(e.message);
+            }
+        }
+    })
+        .catch((err) => {
+            // tslint:disable-next-line
+            console.error(err);
+        });
+} else {
+    if (inputs.size === 0) {
+        // tslint:disable-next-line
+        console.error("Oops! You provided no inputs");
+        // tslint:disable-next-line
+        console.log(`
+You can pipe JSON to this program with the --stdin flag:
+
+    curl http://example.com/some-json | swagger-ts --stdin
+
+Or, provide path names:
+
+    swagger-ts path/to/my-file.json
+        `);
+    } else {
+        const queue = inputs
+            .map((input) => {
+                return {
+                    input,
+                    parsed: parse(input),
+                };
+            })
+            .map((incoming) => {
+                return {
+                    incoming,
+                    resolved: resolveInput(incoming, process.cwd()),
+                };
+            });
+
+        const withErrors = queue.filter((x) => x.resolved.errors.length > 0);
+        const withoutErrors = queue.filter((x) => x.resolved.errors.length === 0);
+        if (withErrors.size) {
+            // tslint:disable-next-line
+            console.log("Sorry, there were errors with your input.");
+            withErrors.forEach(function(item) {
+                // tslint:disable-next-line
+                console.log("");
+                // tslint:disable-next-line
+                console.log(`  ${item.incoming.input}:`);
+                // tslint:disable-next-line
+                console.log("    ", item.resolved.errors[0].error.message);
+            });
+        } else {
+            withoutErrors.map((item) => {
+                return item.resolved.content;
+            })
+                .forEach((json: any) => {
+                    // tslint:disable-next-line
+                    console.log(createDefs(json));
+                });
+        }
+    }
+}
+
+interface IIncomingInput {
+    input: string;
+    parsed: ParsedPath;
+}
+
+interface InputError {
+    kind: string;
+    error: Error;
+}
+
+interface IResolvedInput {
+    errors: InputError[];
+    content?: string;
+}
+
+function resolveInput(incoming: IIncomingInput, cwd): IResolvedInput {
+    const absolute = join(cwd, incoming.parsed.dir, incoming.parsed.base);
+    if (!existsSync(absolute)) {
+        return {
+            errors: [{
+                error: new Error(`File not found`),
+                kind: "FileNotFound",
+            }],
+        };
+    }
+    const data = readFileSync(absolute, "utf8");
+    try {
+        return {
+            content: JSON.parse(data),
+            errors: [],
+        };
+    } catch (e) {
+        return {
+            errors: [{
+                error: e,
+                kind: "InvalidJson",
+            }],
+        };
+    }
+}
